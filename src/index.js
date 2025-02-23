d8bce41e2b1502eea2e33de0bb9ebd3f08d9d291
ayush mhetre <113123869+Volentry@users.noreply.github.com>
2025-02-23 18:48:34 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const {PORT} = require('./config');
+const express = require('express');
+const app = express()
+const apiRoutes = require('./routes')
+app.use('/api',apiRoutes)
+app.listen(PORT,()=>{
+    console.log(`Successfully connected : ${PORT}`)
+})

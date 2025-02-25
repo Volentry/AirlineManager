1b6a4316b9ac05b173debd923a1e858c78db4b1e
ayush mhetre <113123869+Volentry@users.noreply.github.com>
2025-02-26 04:29:18 +0530

    made few innerjoins between relational model with help of seqCLI

--- src/repositories/flight-respository.js
+++ src/repositories/flight-respository.js
@@ -1,5 +1,5 @@
 const CrudRepository = require('./crud-repository')
-const {Flight} = require('../models');
+const {Flight,Airplane,Airport, Sequelize, City} = require('../models');
 const { where } = require('sequelize');
 class FlightRepository extends CrudRepository{
     constructor(){
@@ -8,7 +8,34 @@ class FlightRepository extends CrudRepository{
    async getAllFlights(filter,sortFilter){
     const response  = await Flight.findAll({
         where:filter,
-        order:sortFilter
+        order:sortFilter,
+        include:[{
+            model:Airplane,
+            required:true,
+            as:'airplane_detail'
+        },{
+            model:Airport,
+            required:true,
+            as:'departure_Airport',
+            on:{
+                col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"),'=',Sequelize.col('departure_Airport.code'))
+            },
+            include:{
+                model:City,
+                required:true
+            }
+        },{
+            model:Airport,
+            required:true,
+            as:'arrival_Airport',
+            on:{
+                col1:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"),'=',Sequelize.col('arrival_Airport.code'))
+            },
+            include:{
+                model:City,
+                required:true
+            }
+        }]
     });
         return response
     }
